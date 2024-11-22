55425e7f3f69a095eeeabf07aa5ba970cecc58d5
Eshan_Abeysekara <87514961+Eshan-Abeysekara@users.noreply.github.com>
2024-11-22 12:32:46 +0530

    Create Food-menu

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import '/.Home.css'
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../Components/Header/Header'
+import ExplorMenu from '../../Components/ExplorMenu/ExplorMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 
 const Home = () => {
+
+  const [category, setCategory] = useState("All");
+
   return (
     <div>
-      
+      <Header />
+      <ExplorMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category}/>
     </div>
   )
 }
--- frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/Pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import '/.PlaceOrder.css'
+import './PlaceOrder.css'
 
 const PlaceOrder = () => {
   return (
     <div>
-      
+      <h1> Place Holder </h1>
     </div>
   )
 }
