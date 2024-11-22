07abb993d465f3255ef30f856ab27943c1e46131
Eshan_Abeysekara <87514961+Eshan-Abeysekara@users.noreply.github.com>
2024-11-22 13:43:19 +0530

    update food menu

--- frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -3,19 +3,22 @@ import "./FoodDisplay.css";
 import { StoreContext } from "../../Context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
+
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext);
+  const { food_list } = useContext(StoreContext)
 
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
         {food_list.map((item, index) => {
-          return <FoodItem key={index} id={item.id} name={item.name} description={item.description} image={item.image} />
+          if (category==="All" || category===item.category) {
+            return <FoodItem key={index} id={item.id} name={item.name} description={item.description} image={item.image} price={item.price} category={item.category} />
+          }
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default FoodDisplay;
--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -23,6 +23,7 @@ const StoreContextProvider = (props) => {
   useEffect(()=>{
     console.log(cartItems);
   },[cartItems])
+  
 
   const contextValue = {
     food_list,
